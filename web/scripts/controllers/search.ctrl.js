5d924221ba7df9d80ebe395be30195c7546c86c2
bxav <xavier.b@zoho.com>
2016-02-21 19:10:55 +0100

    Add more fixtures

--- web/scripts/controllers/search.ctrl.js
+++ web/scripts/controllers/search.ctrl.js
@@ -18,4 +18,13 @@ function SearchCtrl(serviceService) {
             vm.services = services;
         })
     }
+
+    vm.getRandomAvatar = function(){
+        var genderArray = [
+            'men',
+            'women'
+        ];
+        var randomNb = Math.floor(Math.random()*genderArray.length);
+        return 'https://randomuser.me/api/portraits/med/'+genderArray[randomNb]+'/' + Math.floor((Math.random() * 100) + 1)+'.jpg';
+    }
 }
