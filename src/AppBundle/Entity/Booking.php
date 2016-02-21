3f40ac8e029df19a21420b2fbee8a899b4a70336
bxav <xavier.b@zoho.com>
2016-02-21 20:50:23 +0100

    Add the possibility to take a booking through the API

---
+++ src/AppBundle/Entity/Booking.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booking.
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Booking
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date")
+     */
+    protected $startDate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=true)
+     **/
+    protected $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ServiceProduct")
+     * @ORM\JoinColumn(name="service_id", referencedColumnName="id", nullable=true)
+     **/
+    protected $service;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    public function setService($service)
+    {
+        $this->service = $service;
+    }
+}
